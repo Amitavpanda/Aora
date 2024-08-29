438259a726301d3523e6aeb2c6da2d7909ca7919
Amitav Panda <amitavpanda@Amitav-ka-MacBook-Pro.local>
2024-08-29 22:56:23 +0530

    add the code to setup

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Link } from "expo-router";
+import { View , Text, StyleSheet} from "react-native";
+
+
+
+
+export default function App(){
+    return(
+       <View className="flex-1 items-center justify-center bg-yellow-400">
+            <Text>Aora</Text>
+       </View>
+    )
+}
---
+++ app/profile.tsx
@@ -0,0 +1,14 @@
+import { Link } from "expo-router";
+import { View , Text, StyleSheet} from "react-native";
+
+
+
+
+export default function Profile(){
+    return(
+        <View className="flex-1 items-center justify-center bg-yellow-400 ">
+            <Text className="text-pink-400">Profile</Text>
+        </View>
+    )
+}
+
